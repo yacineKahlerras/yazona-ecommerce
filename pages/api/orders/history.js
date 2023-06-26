0560ae7406c7ebf2cdf7de5f60a5d55614b197e7
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-06-26 20:21:00 +0100

    testing vercel error

--- pages/api/orders/history.js
+++ pages/api/orders/history.js
@@ -1,11 +1,11 @@
-import { getSession } from 'next-auth/react';
-import Order from '../../../models/Order';
-import db from '../../../utils/db';
+import { getSession } from "next-auth/react";
+import Order from "../../../models/Order";
+import db from "../../../utils/db";
 
 const handler = async (req, res) => {
   const session = await getSession({ req });
   if (!session) {
-    return res.status(401).send({ message: 'signin required' });
+    return res.status(401).send({ message: "signin required" + session });
   }
   const { user } = session;
   await db.connect();
