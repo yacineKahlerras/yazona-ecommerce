ded9511375befd32bc3a35cac0c1cac1d0ef519a
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-06-26 13:14:03 +0100

    removing footer

--- components/Layout.js
+++ components/Layout.js
@@ -148,8 +148,8 @@ export default function Layout({ title, children }) {
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
-          <p>Copyright © 2022 Amazona</p>
+        <footer className="flex h-10 justify-center items-center shadow-inner mt-5">
+          {/* <p>Copyright © 2022 Amazona</p> */}
         </footer>
       </div>
     </>
